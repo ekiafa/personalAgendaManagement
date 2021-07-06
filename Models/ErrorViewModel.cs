4c184710210ba0cb538d18c7dfa434fdcc1fbbd1
ekiafa <kiafaeftychia@gmail.com>
2021-07-06 22:32:39 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace personal_agenda_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
